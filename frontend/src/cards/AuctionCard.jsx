113e7213b7977128de8e588ff775c9f5c7aa979a
snabelA00 <ja000411@gmail.com>
2021-11-02 16:29:38 +0100

    test images and navbar i fixed

--- frontend/src/cards/AuctionCard.jsx
+++ frontend/src/cards/AuctionCard.jsx
@@ -9,10 +9,22 @@ export const AuctionCard = (props) => {
     const goToAuctionDetails = (e) => {
         history.push(`/auction-details/${auction.id}`)
     }
+
+    
+
+
     
     return(
         <div onClick={goToAuctionDetails} className="h-40 flex bg-gray-200">
-            <div className="w-2/5 h-full  bg-yellow-300">Image</div>
+            { true ? (
+                    <div>
+                        <img className="w-2/5 h-full  bg-yellow-300" src="../test-images/plick-sweater.tiff"></img>
+                    </div>
+                ) : (
+                    <div>
+                        <img className="w-2/5 h-full  bg-yellow-300" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"></img>
+                    </div>
+                  )}
             <div className="p-2 w-full bg-green-300">
                 <div className="text-white bg-green-500">{auction.title}</div>
                 <div className="text-white bg-green-600">{auction.description}</div>
