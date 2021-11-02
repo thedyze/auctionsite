9ae6d24ee92b79d52fec2dc0d63dfb806a9eb85b
tripleWdotcom <mespinoza.arias@gmail.com>
2021-11-02 23:33:52 +0100

    #7 Datepicker working
     added a basic object template for new item listing

--- frontend/src/components/Datepicker.jsx
+++ frontend/src/components/Datepicker.jsx
@@ -1,22 +1,25 @@
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
-import { subDays,addDays } from 'date-fns';
+import { subDays, addDays } from "date-fns";
 
+export default function Datepicker({ callback }) {
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
+  const hola = (selectedDate) => {
+    callback(selectedDate);
+    setSelectedDate(selectedDate);
+  };
 
-export default function Datepicker() {
-const [selectedDate, setSelectedDate] = useState(new Date());
-
-  return(
+  return (
     <DatePicker
-    showPopperArrow={false}
-    selected={selectedDate}
-    onChange={(selectedDate) => setSelectedDate(selectedDate)}
-    minDate={subDays(new Date(), -1)}
-    maxDate={addDays(new Date(), 30)}
-    dateFormat="dd/MM yyyy h:mm"
-    placeholderText="Select a date"
+      showPopperArrow={false}
+      selected={selectedDate}
+      onChange={hola}
+      minDate={subDays(new Date(), -1)}
+      maxDate={addDays(new Date(), 30)}
+      dateFormat="dd/MM yyyy h:mm"
+      placeholderText="Select a date"
     />
-  )
-};
+  );
+}
--- frontend/src/pages/CreateListing.jsx
+++ frontend/src/pages/CreateListing.jsx
@@ -1,11 +1,31 @@
+import { useState, useEffect } from "react";
 import Datepicker from "../components/Datepicker";
-import { useState } from "react";
 
 export const CreateListing = () => {
 
-  return(
-  <div>CreateListing
-  <Datepicker />
-  </div>
-  )
+  //Obj used to add the info and create a new listing
+  const [itemObj, setItemObj] = useState({
+    title: "",
+    description: "",
+    endTime: "",
+    categoryId: "",
+    startPrice: "",
+    endTime: new Date().getTime(),
+  });
+
+  const handleChosenDate = (data) => {
+    setItemObj({ ...itemObj, endTime: data.getTime()});
+  };
+
+  useEffect(() => {
+    handleChosenDate;
+    console.log("Updated Info", itemObj);
+  }, [itemObj]);
+
+  return (
+    <div>
+      CreateListing
+      <Datepicker callback={handleChosenDate} />
+    </div>
+  );
 };
