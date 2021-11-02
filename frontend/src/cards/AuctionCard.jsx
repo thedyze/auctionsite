b8eb24c4764d8e0f7b8f9d76484d4e5916b47180
snabelA00 <ja000411@gmail.com>
2021-11-02 10:57:11 +0100

    implemented useHistory hook to navigate to auction details when clicking on an item(AuctionCard)

--- frontend/src/cards/AuctionCard.jsx
+++ frontend/src/cards/AuctionCard.jsx
@@ -1,9 +1,17 @@
+import { useHistory } from "react-router-dom";
+
 export const AuctionCard = (props) => {
 
     const { auction } = props;
+
+    const history = useHistory()
+
+    const goToAuctionDetails = (e) => {
+        history.push(`/auctionItem/${auction.id}`)
+    }
     
     return(
-        <div className="h-40 flex bg-gray-200">
+        <div onClick={goToAuctionDetails} className="h-40 flex bg-gray-200">
             <div className="w-2/5 h-full  bg-yellow-300">Image</div>
             <div className="p-2 w-full bg-green-300">
                 <div className="text-white bg-green-500">{auction.title}</div>
