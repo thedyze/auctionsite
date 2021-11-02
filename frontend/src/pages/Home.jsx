2b2c559e1a5a31b955833d48548e5629235d6f83
lukasbloom93 <72065185+lukasbloom93@users.noreply.github.com>
2021-11-02 11:33:41 +0100

    cleaned up Home page from testing code

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,21 +1,7 @@
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import { useState } from "react";
 export const Home = () => {
   return (
     <div className="bg-gray-700 h-screen">
       <div className="text-yellow-500 text-4xl text-center">Hidden Gems</div>
     </div>
-  );
-};
-export const Datepick = () => {
-  const [startDate, setStartDate] = useState(null);
-  return (
-    <DatePicker
-      selected={startDate}
-      onChange={(date) => setStartDate(date)}
-      minDate={subDays(new Date(), 5)}
-      placeholderText="Select a date after 5 days ago"
-    />
-  );
+  )
 };
