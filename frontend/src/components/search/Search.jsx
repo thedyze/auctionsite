1988bbee4cced2c007b004754acfcca8cfd355ec
tripleWdotcom <mespinoza.arias@gmail.com>
2021-11-03 09:44:57 +0100

    search basic

---
+++ frontend/src/components/search/Search.jsx
@@ -0,0 +1,8 @@
+export const Search = () => {
+  return (
+    <div>
+      <div style={{ textAlign: "center" }}>SEARCH</div>
+      <input type="text" placeholder />
+    </div>
+  );
+};
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,12 @@
+import { Search } from "../components/search/Search";
 import { HomeAuctionList } from "../lists/HomeAuctionList";
 
 export const Home = () => {
   return (
+
     <div className="bg-gray-700 grid place-items-center h-screen">
-      <div className="text-yellow-500 text-4xl text-center">Hidden Gems</div>
+      <div className="text-yellow-500 text-base text-center">Hidden Gems</div>
+    <Search />
       <HomeAuctionList/>
     </div>
   )
