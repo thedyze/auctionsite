e840fd878454e5cd5fe5f5631b1a1df3dd231228
Kristian <the.dyze@gmail.com>
2021-11-03 21:20:54 +0100

    Separate preview for each file
    Co-Authored-By: tripleWdotcom <67017215+tripleWdotcom@users.noreply.github.com>
    Co-Authored-By: Hugo Granath <72066319+mrHugolo@users.noreply.github.com>

--- frontend/src/components/ImageUpload.jsx
+++ frontend/src/components/ImageUpload.jsx
@@ -7,33 +7,31 @@ import { useState } from 'react'
   // })
 // create a holder to store files
 let formData = new FormData()
-console.log("formdata1" + formData)
+
 
 export const ImageUpload = () => {
-  const [preview, setPreview] = useState('')
+  const [preview1, setPreview1] = useState('')
+  const [preview2, setPreview2] = useState('')
+  const [preview3, setPreview3] = useState('')
   // const [images, setImages] = useState('')
-
+  const [counter, setCounter] = useState(1)
 
 
   
 
-  async function onFileLoad(e) {
-    e.preventDefault()
-    // let files = document.getElementById("image1").files
+  async function onFileLoad(e,number) {
+    
+    console.log("number" +number)
+    
+    //let files = document.getElementById("image1").files
     let file = e.target.files[0]
     
     console.log(file);
-
-
-    // add files to formData
-    //  for (let file of files) {
-      
-    let image = new Image()
-    image.src = URL.createObjectURL(file)
+    console.log("counter" + counter)
     
+      let image = new Image()
+      image.src = URL.createObjectURL(file)
     
-    console.log("imagesrc " +image.src)
-
       image.onload = async () => {
         let canvas = document.createElement('canvas')
         let ctx = canvas.getContext('2d')
@@ -49,44 +47,58 @@ export const ImageUpload = () => {
         // change file type to jpg
         formData.append('files', compressedFile, file.name.replace(/\.\w{3,5}$/, '.jpg'))
 
-        // send files to server
-        // let res = await fetch('/api/upload', {
-        //   method: 'POST',
-        //   body: formData
-        // })
-
         // let filePaths = await res.json()
         // console.log(filePaths);
-
-        setPreview(image.src)
+        if (number === 1) {
+          setPreview1(image.src)
+        }
+        if (number === 2) {
+          setPreview2(image.src)
+        }
+        if (number === 3) {
+          setPreview3(image.src)
+        }
+        
+        setCounter(counter+1)
         console.log(formData);
-        // clear input of files
+        
         // e.target.value = ''
-      // }
+     
 
     }
   }
 
-  async function Upload(e) {
+  async function onFilesUpload(e) {
+
     e.preventDefault()
     console.log(formData);
     let res = await fetch('/api/upload', {
       method: 'POST',
       body: formData
     })
+
     let filePaths = await res.json()
     console.log(filePaths);
   }
+
+  // function handlePreview(number) {
+  //     document.getElementById(`image${number}`).src=
+  // }
 
 
 
     return (
       <div>
-        <form className="" onSubmit={Upload}>
-          <input accept="image/*" type="file" id="image1" onChange={onFileLoad} />
-          <input accept="image/*" type="file" id="image2" onChange={onFileLoad} />
+        <form className="" onSubmit={onFilesUpload}>
+          <input accept="image/*" type="file" id="image1" onChange={e => onFileLoad(e,1)} />
+          <img className="max-h-32 " src={preview1} id="image1" alt="" />
+
+          <input accept="image/*" type="file" id="image2" onChange={e => onFileLoad(e, 2)} />
+          <img className="max-h-32 " src={preview2} id="image2" alt="" />
+
+          <input accept="image/*" type="file" id="image3" onChange={e => onFileLoad(e, 3)} />
+          <img className="max-h-32 " src={preview3} id="image3"alt="" />
           
-          <img className="max-h-32 " src={preview} alt="" />
           <button className="bg-blue-500 px-2" type="submit"> Upload Files </button>
         </form>
       </div>
