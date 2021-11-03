f77f3c8ff2dd3a029bd0a854ac0bf85f981b9f03
Kristian <the.dyze@gmail.com>
2021-11-03 13:59:54 +0100

    Random generated string for image filename

--- backend/src/main/java/com/group4/auctionsite/services/UploadService.java
+++ backend/src/main/java/com/group4/auctionsite/services/UploadService.java
@@ -8,6 +8,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class UploadService {
@@ -24,23 +25,34 @@ public class UploadService {
     // get current working directory
     String cwd = System.getProperty("user.dir");
     String uploadFolder = cwd + "/src/main/resources/static/uploads/";
-    int fileNumber = 1;
 
     for (var file : files) {
       System.out.println(file.getOriginalFilename());
 
-      String fileName = Long.toString(loggedInUser.getId())+ "_" + fileNumber +".jpg";
+      int leftLimit = 48; // numeral '0'
+      int rightLimit = 122; // letter 'z'
+      int targetStringLength = 16;
+      Random random = new Random();
+
+      String generatedString = random.ints(leftLimit, rightLimit + 1)
+              .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+              .limit(targetStringLength)
+              .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+              .toString();
+
+      String fileName = generatedString +".jpg";
+//      String fileName = Long.toString(loggedInUser.getId())+ "_" + fileNumber +".jpg";
 
 //      var uploadUrl = "/uploads/" + file.getOriginalFilename();
       var uploadUrl = "/uploads/" + fileName;
 
       // create destination to save uploaded file
       File toSave = new File(uploadFolder + fileName);
-      fileNumber++;
       try {
         // move uploaded to uploads folder
         file.transferTo(toSave);
         uploadUrls.add(uploadUrl);
+        System.out.println(uploadUrls);
 
       } catch (Exception e) {
         e.printStackTrace();
--- frontend/src/components/ImageUpload.jsx
+++ frontend/src/components/ImageUpload.jsx
@@ -1,10 +1,19 @@
 import { useState } from 'react'
 
+// export const ImageUpload = () => {
+//   const [preview, setPreview] = useState('')
+  // const [imgFile, setImgFile] = useState({
+  //   file: '',
+  // })
+
 export const ImageUpload = () => {
   const [preview, setPreview] = useState('')
+  const [images, setImages] = useState('')
 
   async function onFileLoad(e) {
-    let files = e.target.files
+    e.preventDefault()
+    let files = document.getElementById("images").files
+    // let files = e.target.files
     console.log(files);
 
     // create a holder to store files
@@ -12,7 +21,7 @@ export const ImageUpload = () => {
 
     // add files to formData
     for (let file of files) {
-
+      
       let image = new Image()
       image.src = URL.createObjectURL(file)
 
@@ -39,36 +48,114 @@ export const ImageUpload = () => {
         let filePaths = await res.json()
         console.log(filePaths);
 
-        setPreview(filePaths[0])
+         setPreview(filePaths)
         // clear input of files
-        e.target.value = ''
+        // e.target.value = ''
       }
 
     }
 
-  }
-
-  return (
-    <div>
-      <input type="file" id="files" name="files" accept="image/*" multiple onChange={onFileLoad} />
-      <img src={preview} alt="" />
-    </div>
     
-  )
-}
 
-// export default ImageUpload;
-
-// helper function to convert canvas image to file
-// should be in a utility file
-function dataURItoBlob(dataURI) {
-  let byteString = atob(dataURI.split(',')[1]);
-  let mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
-  let ab = new ArrayBuffer(byteString.length);
-  let ia = new Uint8Array(ab);
-  for (let i = 0; i < byteString.length; i++) {
-    ia[i] = byteString.charCodeAt(i);
   }
-  let blob = new Blob([ab], { type: mimeString });
-  return blob;
-}
+    return (
+      <div>
+        <form className="" onSubmit={onFileLoad}>
+          <input accept="image/*" type="file" id="images" onChange={onFileLoad}/>
+          <img src={preview} alt="" />
+          {/* <button className="bg-blue-500 px-2" type="submit"> Upload Files </button> */}
+        </form>
+      </div>
+
+    )
+  }
+
+
+  // export default ImageUpload;
+
+  // helper function to convert canvas image to file
+  // should be in a utility file
+  function dataURItoBlob(dataURI) {
+    let byteString = atob(dataURI.split(',')[1]);
+    let mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+    let ab = new ArrayBuffer(byteString.length);
+    let ia = new Uint8Array(ab);
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+    let blob = new Blob([ab], { type: mimeString });
+    return blob;
+  }
+
+
+
+
+
+
+
+//   }
+
+//   return (
+//     <div>
+//       <input type="file" id="files" name="files" accept="image/*" multiple onChange={onFileLoad} />
+//       <img src={preview} alt="" />
+//     </div>
+
+//   )
+// }
+
+
+// async function onFileLoad(e) {
+    //   e.preventDefault()
+    //   let file = imgFile
+    //   console.log(imgFile.file.name);
+
+    //   // create a holder to store files
+    //   let formData = new FormData()
+
+    //   // add files to formData
+    //   // for (let file of files) {
+
+    //     let image = new Image()
+    //     image.src = URL.createObjectURL(file)
+
+    //     image.onload = async () => {
+    //       let canvas = document.createElement('canvas')
+    //       let ctx = canvas.getContext('2d')
+    //       canvas.width = image.width
+    //       canvas.height = image.height
+
+    //       ctx.drawImage(image, 0, 0)
+
+    //       // compress image to 80% quality
+    //       let compressedFile = dataURItoBlob(canvas.toDataURL('image/jpeg', 0.8))
+
+    //       // change file type to jpg
+    //       formData.append('file', compressedFile, file.name.replace(/\.\w{3,5}$/, '.jpg'))
+
+    //       // send files to server
+    //       let res = await fetch('/api/upload', {
+    //         method: 'POST',
+    //         body: formData
+    //       })
+
+    //       let filePaths = await res.json()
+    //       console.log(filePaths);
+
+    //       setPreview(filePaths[0])
+    //       // clear input of files
+    //       e.target.value = ''
+    //     }
+
+    //   // }
+
+    // }
+
+    // const onFileChange = (e) => {
+    //   // debugger
+    //   setImages({
+    //     images: e.target.files
+    //   })
+
+    // }
+    // value = { imgFile.file }
