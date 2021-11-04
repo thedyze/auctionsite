d8d9fdf9e2e4e73d183265a84b95bc8a16dd26f1
Kristian <the.dyze@gmail.com>
2021-11-04 18:33:53 +0100

    Added try/catch when adding image, old code cleanup

--- frontend/src/components/ImageUpload.jsx
+++ frontend/src/components/ImageUpload.jsx
@@ -18,52 +18,43 @@ export const ImageUpload = () => {
 
 
 
-  async function onFileLoad(e, number) {
+  async function onAddImage(e, number) {
+    try {
+      let file = e.target.files[0]
+      let image = new Image()
+      image.src = URL.createObjectURL(file)
 
-    //let files = document.getElementById("image1").files
+      image.onload = async () => {
+        let canvas = document.createElement('canvas')
+        let ctx = canvas.getContext('2d')
 
-    let file = e.target.files[0]
-    console.log(file);
+        canvas.width = image.width
+        canvas.height = image.height
+        ctx.drawImage(image, 0, 0)
+        let compressedFile = dataURItoBlob(canvas.toDataURL('image/jpeg', 0.6))
 
-    let image = new Image()
-    image.src = URL.createObjectURL(file)
+        if (number === 1) {
+          updateFormData('_img1.jpg', compressedFile)
+          setPreview1(image.src)
+        }
 
-    image.onload = async () => {
-      let canvas = document.createElement('canvas')
-      let ctx = canvas.getContext('2d')
+        if (number === 2) {
+          updateFormData('_img2.jpg', compressedFile)
+          setPreview2(image.src)
+        }
 
-      canvas.width = image.width
-      canvas.height = image.height
-
-      ctx.drawImage(image, 0, 0)
-
-      // compress image to 80% quality
-      let compressedFile = dataURItoBlob(canvas.toDataURL('image/jpeg', 0.6))
-
-      // change file type to jpg
-      // formData.set('files', compressedFile, file.name.replace(/\.\w{3,5}$/, '.jpg'))
-
-      if (number === 1) {
-        updateFormData('_img1.jpg', compressedFile)
-        setPreview1(image.src)
+        if (number === 3) {
+          updateFormData('_img3.jpg', compressedFile)
+          setPreview3(image.src)
+        }
+        // let temp = formData.getAll('files')
+        // console.log("formdata late", temp);
       }
-
-      if (number === 2) {
-        updateFormData('_img2.jpg', compressedFile)
-        setPreview2(image.src)
-      }
-
-      if (number === 3) {
-        updateFormData('_img3.jpg', compressedFile)
-        setPreview3(image.src)
-      }
-
-       let temp = formData.getAll('files')
-       console.log("formdata late", temp);
-
     }
+    catch (e) { console.error(e); }
   }
 
+  
   function updateFormData(imgname, file) {
 
     const tmpData = formData.getAll('files')
@@ -77,10 +68,9 @@ export const ImageUpload = () => {
   }
 
 
-  async function onFilesUpload(e) {
-
+  async function onFilesSubmit(e) {
     e.preventDefault()
-    // console.log(formData);
+
     let res = await fetch('/api/upload', {
       method: 'POST',
       body: formData
@@ -90,43 +80,37 @@ export const ImageUpload = () => {
     console.log(filePaths);
   }
 
-  // function handlePreview(number) {
-  //     document.getElementById(`image${number}`).src=
-  // }
-
-
 
   return (
     <div>
       <div className="w-full p-4 flex justify-center font-medium text-indigo-600">
         Click an image to add
       </div>
-      <form className="" onSubmit={onFilesUpload}>
+      <form className="" onSubmit={onFilesSubmit}>
         <div className="w-full h-32 px-8 grid grid-rows-2 grid-cols-3 gap-4">
 
           <div className="image-upload w-40 h-32 row-span-2 col-span-2">
             <label htmlFor="image1">
               <img className="object-cover max-h-32" src={preview1} alt="" />
             </label>
-            <input id="image1" accept="image/*" type="file" onChange={e => onFileLoad(e, 1)} />
+            <input id="image1" accept="image/*" type="file" onChange={e => onAddImage(e, 1)} />
           </div>
 
           <div className="image-upload">
             <label htmlFor="image2">
               <img className="object-cover max-h-16" src={preview2} alt="" />
             </label>
-            <input accept="image/*" type="file" id="image2" onChange={e => onFileLoad(e, 2)} />
+            <input accept="image/*" type="file" id="image2" onChange={e => onAddImage(e, 2)} />
           </div>
 
           <div className="image-upload">
             <label htmlFor="image3">
               <img className="object-cover max-h-16" src={preview3} alt="" />
             </label>
-            <input accept="image/*" type="file" id="image3" onChange={e => onFileLoad(e, 3)} />
+            <input accept="image/*" type="file" id="image3" onChange={e => onAddImage(e, 3)} />
           </div>
-
         </div >
-        
+
         <button className="bg-blue-500 px-2 mt-6 " type="submit"> Upload Files </button>
       </form>
     </div>
@@ -134,8 +118,6 @@ export const ImageUpload = () => {
   )
 }
 
-
-// export default ImageUpload;
 
 // helper function to convert canvas image to file
 // should be in a utility file
@@ -150,76 +132,3 @@ function dataURItoBlob(dataURI) {
   let blob = new Blob([ab], { type: mimeString });
   return blob;
 }
-
-
-
-
-
-
-
-//   }
-
-//   return (
-//     <div>
-//       <input type="file" id="files" name="files" accept="image/*" multiple onChange={onFileLoad} />
-//       <img src={preview} alt="" />
-//     </div>
-
-//   )
-// }
-
-
-// async function onFileLoad(e) {
-    //   e.preventDefault()
-    //   let file = imgFile
-    //   console.log(imgFile.file.name);
-
-    //   // create a holder to store files
-    //   let formData = new FormData()
-
-    //   // add files to formData
-    //   // for (let file of files) {
-
-    //     let image = new Image()
-    //     image.src = URL.createObjectURL(file)
-
-    //     image.onload = async () => {
-    //       let canvas = document.createElement('canvas')
-    //       let ctx = canvas.getContext('2d')
-    //       canvas.width = image.width
-    //       canvas.height = image.height
-
-    //       ctx.drawImage(image, 0, 0)
-
-    //       // compress image to 80% quality
-    //       let compressedFile = dataURItoBlob(canvas.toDataURL('image/jpeg', 0.8))
-
-    //       // change file type to jpg
-    //       formData.append('file', compressedFile, file.name.replace(/\.\w{3,5}$/, '.jpg'))
-
-    //       // send files to server
-    //       let res = await fetch('/api/upload', {
-    //         method: 'POST',
-    //         body: formData
-    //       })
-
-    //       let filePaths = await res.json()
-    //       console.log(filePaths);
-
-    //       setPreview(filePaths[0])
-    //       // clear input of files
-    //       e.target.value = ''
-    //     }
-
-    //   // }
-
-    // }
-
-    // const onFileChange = (e) => {
-    //   // debugger
-    //   setImages({
-    //     images: e.target.files
-    //   })
-
-    // }
-    // value = { imgFile.file }
