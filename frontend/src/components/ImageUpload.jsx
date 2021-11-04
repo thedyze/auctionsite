6d2c4778945272445d76f030ad9406c7da766c28
Kristian <the.dyze@gmail.com>
2021-11-04 12:38:37 +0100

    Image upload buttons as images/preview images

--- frontend/src/components/ImageUpload.jsx
+++ frontend/src/components/ImageUpload.jsx
@@ -2,68 +2,68 @@ import { useState } from 'react'
 
 // export const ImageUpload = () => {
 //   const [preview, setPreview] = useState('')
-  // const [imgFile, setImgFile] = useState({
-  //   file: '',
-  // })
+// const [imgFile, setImgFile] = useState({
+//   file: '',
+// })
 // create a holder to store files
 let formData = new FormData()
 
 
 export const ImageUpload = () => {
-  const [preview1, setPreview1] = useState('')
-  const [preview2, setPreview2] = useState('')
-  const [preview3, setPreview3] = useState('')
+  const [preview1, setPreview1] = useState('src/images/upload.png')
+  const [preview2, setPreview2] = useState('src/images/upload.png')
+  const [preview3, setPreview3] = useState('src/images/upload.png')
   // const [images, setImages] = useState('')
   const [counter, setCounter] = useState(1)
 
 
-  
 
-  async function onFileLoad(e,number) {
-    
-    console.log("number" +number)
-    
+
+  async function onFileLoad(e, number) {
+
+    console.log("number" + number)
+
     //let files = document.getElementById("image1").files
     let file = e.target.files[0]
-    
+
     console.log(file);
     console.log("counter" + counter)
-    
-      let image = new Image()
-      image.src = URL.createObjectURL(file)
-    
-      image.onload = async () => {
-        let canvas = document.createElement('canvas')
-        let ctx = canvas.getContext('2d')
-        
-        canvas.width = image.width
-        canvas.height = image.height
 
-        ctx.drawImage(image, 0, 0)
+    let image = new Image()
+    image.src = URL.createObjectURL(file)
 
-        // compress image to 80% quality
-        let compressedFile = dataURItoBlob(canvas.toDataURL('image/jpeg', 0.8))
-        
-        // change file type to jpg
-        formData.append('files', compressedFile, file.name.replace(/\.\w{3,5}$/, '.jpg'))
+    image.onload = async () => {
+      let canvas = document.createElement('canvas')
+      let ctx = canvas.getContext('2d')
 
-        // let filePaths = await res.json()
-        // console.log(filePaths);
-        if (number === 1) {
-          setPreview1(image.src)
-        }
-        if (number === 2) {
-          setPreview2(image.src)
-        }
-        if (number === 3) {
-          setPreview3(image.src)
-        }
-        
-        setCounter(counter+1)
-        console.log(formData);
-        
-        // e.target.value = ''
-     
+      canvas.width = image.width
+      canvas.height = image.height
+
+      ctx.drawImage(image, 0, 0)
+
+      // compress image to 80% quality
+      let compressedFile = dataURItoBlob(canvas.toDataURL('image/jpeg', 0.8))
+
+      // change file type to jpg
+      formData.append('files', compressedFile, file.name.replace(/\.\w{3,5}$/, '.jpg'))
+
+      // let filePaths = await res.json()
+      // console.log(filePaths);
+      if (number === 1) {
+        setPreview1(image.src)
+      }
+      if (number === 2) {
+        setPreview2(image.src)
+      }
+      if (number === 3) {
+        setPreview3(image.src)
+      }
+
+      setCounter(counter + 1)
+      console.log(formData);
+
+      // e.target.value = ''
+
 
     }
   }
@@ -87,41 +87,60 @@ export const ImageUpload = () => {
 
 
 
-    return (
-      <div>
-        <form className="" onSubmit={onFilesUpload}>
-          <input accept="image/*" type="file" id="image1" onChange={e => onFileLoad(e,1)} />
-          <img className="max-h-32 " src={preview1} id="image1" alt="" />
-
-          <input accept="image/*" type="file" id="image2" onChange={e => onFileLoad(e, 2)} />
-          <img className="max-h-32 " src={preview2} id="image2" alt="" />
-
-          <input accept="image/*" type="file" id="image3" onChange={e => onFileLoad(e, 3)} />
-          <img className="max-h-32 " src={preview3} id="image3"alt="" />
-          
-          <button className="bg-blue-500 px-2" type="submit"> Upload Files </button>
-        </form>
+  return (
+    <div>
+      <div className="w-full p-4 flex justify-center font-medium text-indigo-600">
+        Click on images to add
       </div>
+      <form className="" onSubmit={onFilesUpload}>
+        <div className="w-full h-32 px-8 grid grid-rows-2 grid-cols-3 gap-4">
 
-    )
+          <div className="image-upload w-40 h-32 row-span-2 col-span-2">
+            <label htmlFor="image1">
+              <img className="object-cover max-h-32" src={preview1} alt="" />
+            </label>
+            <input id="image1" accept="image/*" type="file" onChange={e => onFileLoad(e, 1)} />
+          </div>
+
+          <div className="image-upload">
+            <label htmlFor="image2">
+              <img className="object-cover max-h-16" src={preview2} alt="" />
+            </label>
+            <input accept="image/*" type="file" id="image2" onChange={e => onFileLoad(e, 2)} />
+          </div>
+
+          <div className="image-upload">
+            <label htmlFor="image3">
+              <img className="object-cover max-h-16" src={preview3} alt="" />
+            </label>
+            <input accept="image/*" type="file" id="image3" onChange={e => onFileLoad(e, 3)} />
+          </div>
+
+        </div >
+        
+        <button className="bg-blue-500 px-2 mt-6 " type="submit"> Upload Files </button>
+      </form>
+    </div>
+
+  )
+}
+
+
+// export default ImageUpload;
+
+// helper function to convert canvas image to file
+// should be in a utility file
+function dataURItoBlob(dataURI) {
+  let byteString = atob(dataURI.split(',')[1]);
+  let mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+  let ab = new ArrayBuffer(byteString.length);
+  let ia = new Uint8Array(ab);
+  for (let i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
   }
-
-
-  // export default ImageUpload;
-
-  // helper function to convert canvas image to file
-  // should be in a utility file
-  function dataURItoBlob(dataURI) {
-    let byteString = atob(dataURI.split(',')[1]);
-    let mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
-    let ab = new ArrayBuffer(byteString.length);
-    let ia = new Uint8Array(ab);
-    for (let i = 0; i < byteString.length; i++) {
-      ia[i] = byteString.charCodeAt(i);
-    }
-    let blob = new Blob([ab], { type: mimeString });
-    return blob;
-  }
+  let blob = new Blob([ab], { type: mimeString });
+  return blob;
+}
 
 
 
