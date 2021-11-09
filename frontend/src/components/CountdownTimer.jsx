b357f0cd30d4f43f02afbe597bd5dd16a2e16eba
lukasbloom93 <lukas.bloom@hotmail.se>
2021-11-09 20:13:24 +0100

    fixed nested

--- frontend/src/components/CountdownTimer.jsx
+++ frontend/src/components/CountdownTimer.jsx
@@ -8,47 +8,37 @@ export default function CountdownTimer(auctionEndTime) {
     const [countdown, setCountdown] = useState((endTime - currentTime));
     const [formattedTime, setFormattedTime] = useState(['']);
     
-    let format = [];
-
     const formatTime = () => {
-
+        
+        let format = [];
         let days = Math.floor(countdown / (1000 * 60 * 60 * 24));
         let hours = Math.floor((countdown / (1000 * 60 * 60)) % 24);
         let minutes = Math.floor((countdown / (1000 * 60)) % 60);
         let seconds = Math.floor((countdown / 1000) % 60);
         
-        //check if 0 or below, give "auction expired"
         if (countdown < 0) {
             format = ["Auction expired"]
         }
-        // check if over 1 day, give days and hours
         if(countdown > 86400000){
             format = [days, " days ", hours," hours"]
         };
-
-        // check if over 1h and under 24h, give hours and minutes
         if(countdown > 3600000 && countdown < 86400000){
             format = [hours, " hours ", minutes, " min"]
         };
-
-        // check if over 1min, give minutes and seconds
         if(countdown > 60000 && countdown < 3600000){
             format = [minutes, " min ", seconds, " s"]
         };
-        
-         // check if under 1min, give seconds
          if(countdown <= 60000 && countdown > 0){
             format = [seconds, " s"]
         };
-      return format;
+        return format
     }   
-    
+
     useEffect(() => {
-        const timer = setTimeout(() => {setCountdown(countdown - 1000);}, 1000);
-        setFormattedTime(formatTime());
+        const timer = setTimeout(() => {setCountdown(countdown - 1000); setFormattedTime(formatTime());}, 1000);
         return () => clearTimeout(timer);
     });
-
+    
     return (
         <div>{formattedTime}</div>
     );
