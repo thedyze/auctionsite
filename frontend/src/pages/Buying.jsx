e2d0ac00a7dfe87ff26f1c56bec5f439951595c8
Kristian <the.dyze@gmail.com>
2021-11-16 12:59:52 +0100

    Buying, Selling pages css

--- frontend/src/pages/Buying.jsx
+++ frontend/src/pages/Buying.jsx
@@ -41,60 +41,61 @@ export const Buying = () => {
     
 
   return (
-    <div>
-      <div className="font-bold py-2 mx-3 border-b-4 flex justify-between">
+    <div className="bg-myAw h-full pb-20">
+      <div className="w-full text-center font-myHtext text-4xl pt-10 pb-6">Buying</div>
+      <div className="font-bold font-myHtext text-xl py-2 mx-3 border-b-4 flex justify-between">
         <span>Currently bidding on</span>
-        <button onClick={()=>setToggleBidding(p=>!p)}> {toggleBidding?"Hide":"Show"}</button>
+        <button className="text-base underline" onClick={()=>setToggleBidding(p=>!p)}> {toggleBidding?"Hide":"Show"}</button>
       </div>
       {toggleBidding && currentAuctions.map((item) => (
-        <div className="mx-3 my-2 px-2 py-1 border border-solid" key={item.id} onClick={()=>{history.push(`/auction-details/${item.id}`)}} >
-          <div>{item.title}</div>
-          <div className="flex justify-between text-xs">
-            <div className="bg-myPr-dark px-2">
-              <span className="text-myGr-light">Your bid : </span>
-              <span className="text-white">{item.userBid}</span>
+        <div className="bg-white mx-3 my-2 px-2 py-1 border border-solid" key={item.id} onClick={()=>{history.push(`/auction-details/${item.id}`)}} >
+          <div className="font-myPtext text-lg">{item.title}</div>
+          <div className="w-full flex justify-items-start text-xs py-0.5">
+            <div className="w-3/12 bg-myPr-dark p-px rounded-sm mr-1 text-center">
+              <span className="text-myGr-light">Bid : </span>
+              <span className="text-white">{item.userBid + " €"}</span>
             </div>
-            <div className="bg-myGr-light px-2">
-              <span className="text-myPr-dark">Highest Bid : </span>
-              <span className="text-white">{item.highestBid}</span>
+            <div className="w-4/12 bg-myGr-light p-px mr-1 rounded-sm text-center">
+              <span className="text-black">Current : </span>
+              <span className="text-white">{item.highestBid + " €"}</span>
             </div>
-            <div key={item.id}>{<LocalCountdown className="text-black" auctionEndTime={item.endTime}/>}</div>
+            <div className="w-5/12 text-right" key={item.id}>{<LocalCountdown className="text-black" auctionEndTime={item.endTime}/>}</div>
           </div>
         </div>
       ))}
       <br />
-       <div className="font-bold py-2 mx-3 border-b-4 flex justify-between">
+      <div className="font-bold font-myHtext text-xl py-2 mx-3 border-b-4 flex justify-between">
         <span>Won Auctions</span>
-        <button onClick={()=>setToggleWon(p=>!p)}> {toggleWon?"Hide":"Show"}</button>
+        <button className="text-base underline" onClick={()=>setToggleWon(p=>!p)}> {toggleWon?"Hide":"Show"}</button>
       </div>
       {toggleWon && wonAuctions.map((item) => (
-        <div className="mx-3 my-2 px-2 py-1 border border-solid bg-gray-200" key={item.id}>
-          <div className="text-xs flex justify-between text-gray-500">
-            <div className="whitespace-nowrap overflow-x-hidden font-bold overflow-ellipsis" style={{maxWidth:"150px"}} >{item.title}</div>
+        <div className="mx-3 my-2 px-2 py-1 border border-solid bg-gray-200 rounded-sm" key={item.id}>
+          <div className="text-sm flex justify-between text-gray-500">
+            <div className="font-myPtext whitespace-nowrap overflow-x-hidden font-bold overflow-ellipsis" style={{maxWidth:"150px"}} >{item.title}</div>
             <div className="flex justify-end">
-              <div> {new Date(+item.endTime).toLocaleDateString()}  </div>
-              <div className="bg-gray-500 px-2">
-               <span className="text-black">Price : </span>
-               <span className="text-white font-bold">{item.highestBid}</span>
+              <div className="text-xs mr-2 leading-5"> {new Date(+item.endTime).toLocaleDateString()}  </div>
+              <div className="w-20 bg-gray-400 p-px pl-1 text-left rounded-sm">
+                <span className="text-xs font-normal text-black">Paid:  </span>
+                <span className="text-xs text-white font-bold">{item.highestBid + " €"}</span>
               </div>
             </div>
           </div>
         </div>
       ))}
       <br />
-       <div className="font-bold py-2 mx-3 border-b-4 flex justify-between">
+      <div className="bg-myAwfont-bold font-myHtext text-xl pb-2 mt-4 mx-3 border-b-4 flex justify-between ">
         <span>Lost Auctions</span>
-        <button onClick={()=>setToggleLost(p=>!p)}> {toggleLost?"Hide":"Show"}</button>
+        <button className="text-base underline" onClick={()=>setToggleLost(p=>!p)}> {toggleLost?"Hide":"Show"}</button>
       </div>
       {toggleLost && lostAuctions.map((item) => (
-        <div className="mx-3 my-2 px-2 py-1 border border-solid bg-gray-200" key={item.id}>
-          <div className="text-xs flex justify-between text-gray-500">
-            <div>{item.title}</div>
+        <div className="text-sm mx-3 my-2 px-2 py-1 border border-solid bg-gray-200 rounded-sm" key={item.id}>
+          <div className=" whitespace-nowrap overflow-x-hidden font-bold overflow-ellipsis flex justify-between text-gray-500">
+            <div className="font-myPtext font-bold" style={{ maxWidth: "150px" }}>{item.title}</div>
             <div className="flex justify-end">
-              <div> {new Date(+item.endTime).toLocaleDateString()}  </div>
-              <div className="bg-gray-500 px-2">
-               <span className="text-black">Price : </span>
-               <span className="text-white font-bold">{item.highestBid}</span>
+              <div className="text-xs mr-2 font-normal leading-5"> {new Date(+item.endTime).toLocaleDateString()}  </div>
+              <div className="w-20 bg-gray-400 p-px pl-1 text-left rounded-sm">
+                <span className="text-xs font-normal text-black">Price: </span>
+                <span className="text-xs text-white font-bold">{item.highestBid + " €"}</span>
               </div>
             </div>
           </div>
