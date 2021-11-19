df7ac41cd8803e363527f61a7e7cf141b8e0db1d
tripleWdotcom <mespinoza.arias@gmail.com>
2021-11-19 15:09:21 +0100

    border of search input changed to purple to match navbar

--- frontend/src/components/search/SearchInput.jsx
+++ frontend/src/components/search/SearchInput.jsx
@@ -25,15 +25,16 @@ export const SearchInput = ({  handleFilters }) => {
   };
 
   return (
-    <div >
+    <div>
       {/* {state && <div>Loading....</div>} */}
-      <Box sx={{ width: 300, maxWidth: "100%",  }}>
+      <Box sx={{ width: 300, maxWidth: "100%" }}>
         <TextField
-          className="bg-white  pb-8 h-14 rounded-md justify-center align-middle"
+          className="bg-white pb-8 h-14 rounded-md justify-center align-middle"
           color="info"
           fullWidth
           label="Search"
           onChange={handleSearch}
+          color="secondary"
         />
       </Box>
     </div>
